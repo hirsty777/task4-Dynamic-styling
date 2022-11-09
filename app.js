e293c920a1d8df024afd6c51ded18e93cb3bb7e4
hirsty777 <777hirsty@gmail.com>
2022-11-09 22:47:23 +0400

    finished

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const app = Vue.createApp({
-
-
-
+    data(){
+        return{
+            className:'',
+            background:'',
+            isHidden:false,
+        }
+    },
+    methods:{
+        toggleVisibility(){
+            this.isHidden=!this.isHidden
+        }
+    }
 });
 app.mount('#assignment');
